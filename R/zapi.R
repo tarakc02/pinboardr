14ba5613a7056f7d24aaad978260929171db786e
tarakc02 <tarakc02@github.com>
2017-08-20 17:00:18 -0700

    document overall API

--- R/zapi.R
+++ R/zapi.R
@@ -1,3 +1,37 @@
+#' Use the Pinboard API
+#'
+#' \code{posts} and \code{tags} implement the associated endpoints in the
+#' Pinboard API documentation (\url{https://pinboard.in/api/}). See the links
+#' below for documentation of the individual functions.
+#'
+#' @examples
+#' \dontrun{
+#' # to view all bookmarks with the "rstats" tag
+#' posts$dates(tags = "rstats")
+#' }
+#'
+#' @section Posts:
+#' \itemize{
+#' \item{\code{add}: \code{\link{add_post}}}
+#' \item{\code{delete}: \code{\link{delete_post}}}
+#' \item{\code{update}: \code{\link{last_updated}}}
+#' \item{\code{all}: \code{\link{all_posts}}}
+#' \item{\code{recent}: \code{\link{recent_posts}}}
+#' \item{\code{get}}: \code{\link{get_posts}}
+#' \item{\code{suggest}}: \code{\link{suggest}}
+#' }
+#'
+#' @section Tags:
+#' \itemize{
+#' \item{\code{get}: \code{\link{get_tags}}}
+#' \item{\code{delete}: \code{\link{delete_tag}}}
+#' \item{\code{rename}: \code{\link{rename_tag}}}
+#' }
+#'
+#' @name api
+NULL
+
+#' @rdname api
 #' @export
 posts <- list(
     add = add_post,
@@ -10,6 +44,7 @@ posts <- list(
     suggest = suggest
 )
 
+#' @rdname api
 #' @export
 tags <- list(
     get = get_tags,
